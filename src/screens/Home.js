555cdbeee736fac67fec5257353f521082913e09
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2022-07-05 22:04:17 +0700

    layout

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -7,7 +7,10 @@ function Home() {
 
   return (
     <div>
-      <h5>Hi, welcome back {location.state.email}</h5>
+      <h5>Hi, welcome back {location.state.email}!</h5>
+      <h1>My Budget Planner</h1>
+      <h3>Expenses</h3>
+      <h3>Add Expenses</h3>
     </div>
   );
 }
